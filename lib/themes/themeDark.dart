832fdcf4296a0d2f56f7a31ad61d203d3e2fe331
Muxomor <7baltika9@gmail.com>
2024-03-04 11:02:59 +0300

    Update themeDark.dart

--- lib/themes/themeDark.dart
+++ lib/themes/themeDark.dart
@@ -4,27 +4,27 @@ ThemeData themes = ThemeData(
     scaffoldBackgroundColor: const Color(0xff404040),
     elevatedButtonTheme: ElevatedButtonThemeData(
         style: ButtonStyle(
-          foregroundColor: const MaterialStatePropertyAll(Colors.white),
+            foregroundColor: const MaterialStatePropertyAll(Colors.white),
             backgroundColor: const MaterialStatePropertyAll(Colors.blueGrey),
             shape: MaterialStatePropertyAll(RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10))))),
-                outlinedButtonTheme: OutlinedButtonThemeData(
-                  style: ButtonStyle(
+    outlinedButtonTheme: OutlinedButtonThemeData(
+        style: ButtonStyle(
             foregroundColor: const MaterialStatePropertyAll(Colors.blueGrey),
             shape: MaterialStatePropertyAll(RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10))))),
-                appBarTheme:const AppBarTheme(
-                  backgroundColor:Color(0xff404040),
-                  centerTitle: true,
-
-                ),
-                bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-                  selectedItemColor:Colors.white ,
-                  backgroundColor :Color(0xff404040),
-                ),
-                cardTheme: CardTheme(
-                  color: Colors.blueGrey,
-                  shape:OutlineInputBorder(borderRadius:BorderRadius.circular(10)),
-                
-                )
-                  );
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Color(0xff404040),
+      centerTitle: true,
+    ),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      selectedItemColor: Colors.white,
+      backgroundColor: Color(0xff404040),
+    ),
+    cardTheme: CardTheme(
+      color: Colors.blueGrey,
+      shape: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10)
+        ),
+    ),
+    );
