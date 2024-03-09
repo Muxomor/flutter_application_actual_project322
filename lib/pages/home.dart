b2caa29eede9c248caa7385a8d0a72104af6e4f8
Muxomor <7baltika9@gmail.com>
2024-03-09 08:21:25 +0300

    extentions added

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_application_actual_project322/pages/bottom_pages/menu.dart';
 import 'package:flutter_application_actual_project322/pages/bottom_pages/order.dart';
@@ -101,7 +99,7 @@ class profilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+
     );
   }
 }
