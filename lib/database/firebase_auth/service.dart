f2008468976d92fa7def762949debb706d6d6a7d
Muxomor <7baltika9@gmail.com>
2024-03-18 10:07:20 +0300

    new register method(idk why)

--- lib/database/firebase_auth/service.dart
+++ lib/database/firebase_auth/service.dart
@@ -1,41 +1,42 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_application_actual_project322/database/firebase_auth/model.dart';
 
-class AuthService{
- final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+class AuthService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
- Future<userModel?> signIn(String email,String password) async{
-  try{
-    UserCredential userCredential = await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+  Future<userModel?> signIn(String email, String password) async {
+    try {
+      UserCredential userCredential = await _firebaseAuth
+          .signInWithEmailAndPassword(email: email, password: password);
 
-    User user = userCredential.user!;
+      User user = userCredential.user!;
 
-    return userModel.fromFirebase(user);
+      return userModel.fromFirebase(user);
+    } catch (e) {
+      return null;
+    }
   }
-  catch(e){
-    return null;
+
+  Future<userModel?> signUp(String email, String password) async {
+    try {
+      UserCredential userCredential = await _firebaseAuth
+          .createUserWithEmailAndPassword(email: email, password: password);
+
+      User user = userCredential.user!;
+
+      return userModel.fromFirebase(user);
+    } catch (e) {
+      return null;
+    }
   }
- }
 
- Future<userModel?> signUp(String email,String password) async{
-  try{
-    UserCredential userCredential = await _firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
-
-    User user = userCredential.user!;
-
-    return userModel.fromFirebase(user);
-  }catch(e){
-    return null;
+  Future<void> logOut() async {
+    return await _firebaseAuth.signOut();
   }
- }
 
- Future<void> logOut() async{
-  return await _firebaseAuth.signOut();
- }
-
- Stream<userModel?> get currentUser{
-  return _firebaseAuth
-  .authStateChanges()
-  .map((user) => user != null ? userModel.fromFirebase(user):null);
- }
+  Stream<userModel?> get currentUser {
+    return _firebaseAuth
+        .authStateChanges()
+        .map((user) => user != null ? userModel.fromFirebase(user) : null);
+  }
 }
