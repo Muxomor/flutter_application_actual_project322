0256527168635c263b3b21a458ba6a3a5bf13b4f
Muxomor <7baltika9@gmail.com>
2024-04-14 17:49:20 +0300

    trying to implement imagePicker

--- lib/pages/bottom_pages/profile.dart
+++ lib/pages/bottom_pages/profile.dart
@@ -1,14 +1,10 @@
-
 import 'dart:io';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_application_actual_project322/database/firebase_auth/user_collection.dart';
 import 'package:flutter_application_actual_project322/database/image.dart';
-
 import 'package:image_picker/image_picker.dart';
 import 'package:toast/toast.dart';
 
@@ -21,9 +17,11 @@ class ProfilePage extends StatefulWidget {
 
 class ProfilePageState extends State<ProfilePage> {
   dynamic userDoc;
-  final String userId = FirebaseAuth.instance.currentUser!.uid;
+  final String userId = FirebaseAuth.instance.currentUser!.uid.toString();
+
   File? _selectedFile;
-  XFile? _fileName; 
+  XFile? _fileName;
+
   ImageStorage imageStorage = ImageStorage();
   UsersCollection users = UsersCollection();
 
@@ -38,7 +36,8 @@ class ProfilePageState extends State<ProfilePage> {
   }
 
   SelectImageGallery() async {
-    final returnedimage = await ImagePicker().pickImage(source: ImageSource.gallery);
+    final returnedimage =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
     setState(() {
       _fileName = returnedimage;
       _selectedFile = File(returnedimage!.path);
@@ -46,10 +45,13 @@ class ProfilePageState extends State<ProfilePage> {
   }
 
   UpdateInfo() async {
-    if(_fileName != null || nameController.text != ""){
-      String userName = nameController.text != "" ? nameController.text : userDoc['name'];
-      String imagePath = imageStorage.urlImage != null ? imageStorage.urlImage : userDoc['image'];
-      users.eidtUserCollection(userDoc,userName, imagePath);
+    if (_fileName != null || nameController.text != "") {
+      String userName =
+          nameController.text != "" ? nameController.text : userDoc['name'];
+      String imagePath = imageStorage.urlImage != null
+          ? imageStorage.urlImage
+          : userDoc['image'];
+      users.eidtUserCollection(userDoc, userName, imagePath);
       setState(() {
         userDoc = GetUserById();
         nameController.clear();
@@ -65,7 +67,6 @@ class ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       body: Center(
         child: Column(
@@ -79,16 +80,17 @@ class ProfilePageState extends State<ProfilePage> {
                     ? IconButton(
                         onPressed: () async {
                           SelectImageGallery();
-                          await imageStorage.pushImage(_fileName!);
-                          showDialog(
-                            context: context,
-                            builder: (context) => const Center(
-                              child: CircularProgressIndicator(),
-                            ),
-                          );
-                          Future.delayed(const Duration(seconds: 4));
-                          await users.eidtUserCollection(userDoc.id,nameController.text,imageStorage.urlImage!);
-                          Navigator.pop(context);
+                          //await imageStorage.pushImage(_fileName!);
+                          //showDialog(
+                          //context: context,
+                          //builder: (context) => const Center(
+                          //child: CircularProgressIndicator(),
+                          //),
+                          //);
+                          //Future.delayed(const Duration(seconds: 4));
+                          //await users.eidtUserCollection(userDoc.id,
+                          //  nameController.text, imageStorage.urlImage!);
+                          //Navigator.pop(context);
                         },
                         icon: const Icon(
                           Icons.add,
@@ -101,20 +103,43 @@ class ProfilePageState extends State<ProfilePage> {
               ),
             ),
             Spacer(),
-              Text("Никнейм ${userDoc['name']}", style: const TextStyle(color: Colors.white), ),
-              Text("Email ${userDoc['email']}", style: const TextStyle(color: Colors.white),),
-              TextField(              
-                controller: nameController,  
-                decoration: const InputDecoration(
-                  labelText: 'Новый никнейм',
-                  hintText: 'никнейм',
-                  hintStyle:TextStyle(
-                    color: Colors.white54,
-                  ),
+            Text(
+              "Никнейм ${userDoc['name']}",
+              style: const TextStyle(color: Colors.white),
+            ),
+            Text(
+              "Email ${userDoc['email']}",
+              style: const TextStyle(color: Colors.white),
+            ),
+            TextField(
+              controller: nameController,
+              decoration: const InputDecoration(
+                labelText: 'Новый никнейм',
+                hintText: 'никнейм',
+                hintStyle: TextStyle(
+                  color: Colors.white54,
                 ),
               ),
-              OutlinedButton(onPressed: UpdateInfo, child: const Text("Изменить"))
-            ],
+            ),
+            OutlinedButton(
+                onPressed: () async {
+                  showDialog(
+                    context: context,
+                    builder: (context) => const Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                  await imageStorage.pushImage(_fileName!);
+
+                  Future.delayed(const Duration(seconds: 4));
+
+                  await users.eidtUserCollection(userDoc['uid'], nameController.text, imageStorage.urlImage!);
+
+                  Navigator.pop(context);
+                  Toast.show('Успеховый');
+                },
+                child: const Text("Изменить"))
+          ],
         ),
       ),
     );
